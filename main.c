8e0dcd358cef40af6e362e07adb41567dc0bf23e
JT <91373262+jtgit2021@users.noreply.github.com>
2024-11-08 14:31:27 +0100

    Update main.c
    cleaned up code

--- main.c
+++ main.c
@@ -1,12 +1,6 @@
 
-//for base registers look at 2.3 Memory Map , p 67
-
+//for base registers look at 2.3 Memory Map, p 67
 //define addresses for GPIO PG6 other ports etc
-
- // __GPIOI_CLK_ENABLE();  = enable clock
-
-/*gpio mode, pull, speed
-*/
 
 //typecasting pointers
 #define GPIO_PORTG						0x40021800U
@@ -18,55 +12,37 @@
 #define GPIO_PORTG_BSRR				(*(volatile unsigned long *)(GPIO_PORTG + 0x18U))
 #define RCC										0x40023800U
 #define RCC_AHB1ENR						(*(volatile unsigned long *) (RCC+0x30U))	 
-#define RCC_AHB1LPENR					(*(volatile unsigned long *) (RCC+0x50U))	
-
-//RCC_AHB1ENR_GPIOFEN
 
 void portg_init(void);
 void toggle_pin6(void);
 void delay(void);
 
 int main (void){
-	
 	portg_init();
 	
 	while(1){
-		delay(); // gets skipped during execution?
-		toggle_pin6();//pg6 toggle
-		
+		delay();
+		toggle_pin6();
 	}
-	
 }
 
-static int i;
+
 void delay(void){
-		i = 0;
+		static volatile int i; //short delay if not static if i has to be initialized every time
+		i=0;
 
 		while (i<100000) {
 			i++;
 		}
-	
 }
 
-void toggle_pin6(void){
-		
+void toggle_pin6(void){	
     GPIO_PORTG_ODR ^= (1U << 6);  // Toggle LED
-
 }
 
 void portg_init(void){
-			RCC_AHB1ENR |= 0x40U; //set clock for port g
-			//RCC_AHB1LPENR |= 0x40U; // low power clock for port g
-			//delay();
-
-			//GPIO_PORTG_MODER = 0x00; // NEW LINE, reset bits
-			GPIO_PORTG_MODER &= 0xFFFFCFFF; 
-			GPIO_PORTG_MODER |= 0x1000U;  // set portg as output
-			//GPIO_PORTG_OTYPER = 0x0U; // all pins push-pull
-			//GPIO_PORTG_OSPEEDR = 0x0U; // all pins low speed
-	
-			//GPIO_PORTG_PUPDR = 0x2000U; // set pull-down for pin 6
-			
-			GPIO_PORTG_ODR |= 0x40U; // set pin 6 to high?
+		RCC_AHB1ENR |= 0x40U; //set clock for port g
+		GPIO_PORTG_MODER &= 0xFFFFCFFF; // clear bits in portg
+		GPIO_PORTG_MODER |= 0x1000U;  // set portg as output
+		GPIO_PORTG_ODR |= 0x40U; // set pin 6 to high
 }
-
